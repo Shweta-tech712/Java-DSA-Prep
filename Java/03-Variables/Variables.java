90e783104fb8082dd0af23a2799536106cfd60b4
Shweta-tech712 <itzzshwetu@gmail.com>
2026-07-31 17:59:58 +0530

    Add variables example

---
+++ Java/03-Variables/Variables.java
@@ -0,0 +1,19 @@
+public class Variables {
+    public static void main(String[] args) {
+        
+        int age = 21;
+        String name = "Shweta";
+        double cgpa = 8.5;
+        char grade = 'A';
+        boolean isStudent = true;
+
+
+        System.out.println("Name: " + name);
+        System.out.println("Age: " + age);
+        System.out.println("CGPA: " + cgpa);
+        System.out.println("Grade: " + grade);
+        System.out.println("Student: " + isStudent);
+
+    }
+    
+}
