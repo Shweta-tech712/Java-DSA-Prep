c367e0030e6bf9a24d3def2b913acbf852f72a90
Shweta-tech712 <itzzshwetu@gmail.com>
2026-07-31 17:18:08 +0530

    Add Primitive Data Types example

---
+++ Java/02-PrimitiveDataTypes/PrimitiveDataTypes.java
@@ -0,0 +1,26 @@
+public class PrimitiveDataTypes {
+
+    public static void main(String[] args) {
+
+        byte age = 21;
+        short year = 2026;
+        int salary = 500000;
+        long population = 8000000000L;
+
+        float temperature = 35.8f;
+        double pi = 3.14159265359;
+
+        char grade = 'A';
+
+        boolean failed = false;
+
+        System.out.println("Byte: " + age);
+        System.out.println("Short: " + year);
+        System.out.println("Int: " + salary);
+        System.out.println("Long: " + population);
+        System.out.println("Float: " + temperature);
+        System.out.println("Double: " + pi);
+        System.out.println("Char: " + grade);
+        System.out.println("Boolean: " + failed);
+    }
+}
