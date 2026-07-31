36995a407f5320c4119c4fb59d34dc3b4b67f135
Shweta-tech712 <itzzshwetu@gmail.com>
2026-07-31 19:54:44 +0530

    Add Assignment Operators Example

---
+++ Java/05-AssignmentOperators/AssignmentOperators.java
@@ -0,0 +1,23 @@
+public class AssignmentOperators {
+    public static void main(String[] args ) {
+        int number = 20;
+
+        System.out.println("Initial Value : " + number);
+        
+        number += 10;
+        System.out.println("After += 10 : " + number);
+
+        number -= 5;
+        System.out.println("After -= 5 : " + number);
+
+        number *= 2;
+        System.out.println("After *= 2 : " + number);
+
+        number /= 5;
+        System.out.println("After /= 5 : " + number);
+
+        number %= 3;
+        System.out.println("After %= 3 : " + number);
+
+    }
+}
