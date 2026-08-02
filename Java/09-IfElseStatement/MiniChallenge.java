44982b7af6e3909827ecab56c352d436add65c3e
Shweta-tech712 <itzzshwetu@gmail.com>
2026-08-02 22:52:44 +0530

    Added Mini Challenge code along with Readme

---
+++ Java/09-IfElseStatement/MiniChallenge.java
@@ -0,0 +1,12 @@
+public class MiniChallenge {
+    public static void main( String[] args ) {
+
+        boolean loginSuccessful = true;
+
+        if(loginSuccessful){
+           System.out.println("Login Successful");
+        } else{
+           System.out.println("Invalid Username or Password");
+        }
+    }
+}
