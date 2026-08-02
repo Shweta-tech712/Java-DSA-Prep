35d9fb24b06c660da339511022c9c5f93bae7d6f
Shweta-tech712 <itzzshwetu@gmail.com>
2026-08-02 14:17:24 +0530

    Added Relational Operators Example along With Practice Questions and answer

---
+++ Java/06-RelationalOperators/PracticeExample01.java
@@ -0,0 +1,16 @@
+public class PracticeExample01 {
+    public static void main( String [] args ) {
+
+        int marks = 80;
+
+        System.out.println("Marks : " + marks);
+
+        System.out.println(" Marks > 50 : " + (marks > 50));
+        System.out.println(" Marks < 90 : " + (marks < 90));
+        System.out.println(" Marks >= 100 : " + (marks >= 100));
+        System.out.println(" Marks <= 55 : " + (marks <= 55));
+        System.out.println(" Marks != 80 : " + (marks != 80));
+        System.out.println(" Marks == 70 : " + (marks == 70));
+
+    }
+}
---
+++ Java/06-RelationalOperators/PracticeExample02.java
@@ -0,0 +1,13 @@
+public class PracticeExample02 {
+    public static void main( String [] args ) {
+
+        int age = 21;
+
+        System.out.println("Age < 18 : " + (age < 18));
+        System.out.println("Age > 20 : " + (age > 20));
+        System.out.println("Age <= 21 : " + (age <= 21));
+        System.out.println("Age >= 25 : " + (age >= 25));
+        System.out.println("Age == 21 : " + (age == 21));
+        System.out.println("Age != 30 : " + (age != 30));
+    }
+}
